608e1f798ce22feba8f8991b05f4302c0d0a7d77
Helvio Junior <helvio_junior@hotmail.com>
2025-09-05 00:25:34 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Frida Fusion (PT-BR)
+
+Esta é uma para melhorar a experiência com a utilização dos Scripts Frida.
+
+## Instalação
+
+> :information_source: Recomendamos a utilização do `pipx` ao invés do `pip` para instalação no sistema.
+
+```
+python3 -m pipx install frida-fusion
+```
+
+> :information_source: Verifique a necessidade de executar também o comando `python3 -m pipx ensurepath`
---
+++ tests/tests.py
@@ -0,0 +1,25 @@
+#reference: https://medium.com/assertqualityassurance/tutorial-de-pytest-para-iniciantes-cbdd81c6d761
+import codecs
+import os
+import shutil
+import struct
+from pprint import pprint
+import pytest, sys
+
+from ..frida_fusion.libs.color import Color
+
+
+def test_01_none():
+    if sys.stdout.encoding is None:
+        # Output is redirected to a file
+        sys.stdout = codecs.getwriter('latin-1')(sys.stdout)
+
+    try:
+        pass
+
+        assert True
+    except Exception as e:
+        Color.pl('\n{!} {R}Error:{O} %s{W}' % str(e))
+
+        assert False
+
